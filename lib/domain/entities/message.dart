5a70ceb908a4f84489533a0c84f255fc8b423493
Victor-Cauich <victorarturo1004@hotmail.com>
2024-10-28 12:52:46 -0600

    Empezando ChatProvider

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,11 @@
+//  Identidad atomica: la unidad mas pequeña de un sistema
+enum FromWho{me, hers}
+
+class Message {
+  final String text;
+  final String? imageUrl;
+  final FromWho fromWho;
+
+  Message({required this.text, this.imageUrl, required this.fromWho});
+}
+
---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app_victor_4sa/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier{
+
+  List<Message> messageList = [
+    Message(text: 'Hola Ader', fromWho: FromWho.me),
+    Message(text: 'Lloras porque reprobaste ttopicos?', fromWho: FromWho.me)
+  ];
+  // Controlador para manejar la posicion del scroll
+  final ScrollController chatScrollController = ScrollController();
+
+
+  //Enviar un mensaje
+  Future<void> sendMessage(String text) async {
+    //El mensaje siempre va a ser "me" porque yo lo envio
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    //Agrega un elemento a la Lista "messageist"
+    messageList.add(newMessage);
+    // Notifica si algo del provider cambio para que se guarde en el estado
+    notifyListeners();
+    // mueve el scroll
+    moveScrollToBottom();
+  }
+  
+    //Mover el scroll al ultimo mensaje
+
+    Future<void> moveScrollToBottom() async{
+      // Un pequeño atraso en la animacion para garantizar que siempre 
+      // se vera aun cuando se envien mensajes cortos y rapidos
+      await Future.delayed(const Duration(seconds: 1));
+      chatScrollController.animateTo(
+      // maxScrollExtent determina a lo maximo que el scroll puede dar
+        chatScrollController.position.maxScrollExtent, 
+      // Duracion de la animacion
+        duration: const Duration(milliseconds: 300),
+      // "Rebote" al final de la animacion
+         curve: Curves.easeOut);
+  }
+}
